7706964a263cd0e77c43a7dfeaf13f162973577a
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2019-10-13 23:38:43 -0300

    Feature/style (#9)
    * style for pinterest grid
    
    * Search bar style ready
    
    Add favicon
    Trasnform Logo
    Size search bar
    
    * Config Heroku, Style to photos Grid Component

--- src/app/modules/home/photos-grid/photos-grid.component.ts
+++ src/app/modules/home/photos-grid/photos-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { SearchService } from 'src/app/service/search.service';
 
 @Component({
@@ -9,16 +9,32 @@ import { SearchService } from 'src/app/service/search.service';
 export class PhotosGridComponent implements OnInit {
   search: string;
   photos: any[];
+  innerWidth: any;
   constructor(private searchService: SearchService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.showSearch('hola');
+    this.onResize();
+  }
+
+  // Detect window size
+  @HostListener('window:resize')
+  onResize() {
+    this.innerWidth = window.innerWidth;
+  }
 
   showSearch(search: string) {
     this.search = search;
-    this.searchService.getPhotos(search).subscribe((photos: any) => {
-      console.log(photos);
-      this.photos = photos.results;
-    });
-    // this.photos = this.searchService.getPhotos(search);
+    // this.searchService.getPhotos(search).subscribe((photos: any) => {
+    //   console.log(photos);
+    //   this.photos = photos.results;
+    // });
+    this.photos = this.searchService.getPhotos(search);
+    console.log(this.photos);
+  }
+
+  // Función para calcular el número de columnas dentro de la pantalla según el tamaño de las misnma
+  numberOfColumns() {
+    return Math.floor(this.innerWidth / 300);
   }
 }
